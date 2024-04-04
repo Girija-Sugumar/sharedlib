333a16196c38c67c4ef01cced5b4f118b508cce4
Girija-Sugumar <girija.sukumar@softsuave.org>
2024-04-04 17:26:19 +0530

    changes4

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/env groovy
 def call(){
   sh 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
-  sh 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'
+  sh 'source ~/.bashrc'
   sh 'nvm -v'
-  sh 'nvm install v20'
-  sh 'npm install'
 }
